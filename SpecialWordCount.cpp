333a6bbcc7eba85c77dee7e3fbdaeb4e0694309e
VoidUday <udaypratapyadaw03@gmail.com>
2021-01-05 00:26:21 +0530

    Special Word count

--- SpecialWordCount.cpp
+++ SpecialWordCount.cpp
@@ -2,14 +2,23 @@
 using namespace std;
 int main(){
     string s1;
+    int u=0, l=0, s=0;
     cin >> s1;
     for (int i = 0; i < s1.length(); i++)
     {
-        if (s1[i] ==)
+        if (s1[i] >=65 && s1[i] <=90)
         {
-            /* code */
+            u++;
         }
-        
+        else if (s1[i]>=97 && s1[i]<=122)
+        {
+            l++;
+        }
+        else
+        {
+            s++;
+        }
     }
-    
+    cout << "Upper case" << " " << "lower case" << " " << " " << "Special char" << " "<< u << " " << l << " " << s;
+    return 0; 
 }
