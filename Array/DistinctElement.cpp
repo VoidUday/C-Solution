4712bb2de532101a2eaba9e520f1262ac296176d
VoidUday <udaypratapyadaw03@gmail.com>
2021-01-08 00:41:43 +0530

    Array

---
+++ Array/DistinctElement.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+// int  printDistinct(int a[], int n){
+//     int res = 1;
+//     for (int i = 1; i < n; i++)
+//     {
+//        int j =0;
+//        for ( j = 0; j< i; j++)
+//        {
+//            //check if all element are already printed
+//            if (a[i] == a[j])
+//            {
+//                break;
+//            }
+//            // if not printed earlier, then print it;
+//            if (i == j)
+//            {
+//               res++;
+//            } 
+//            return res;
+//        }
+       
+//     }
+    
+// }
+int main(){
+    int n1,i;
+    cin >> n1;
+       int a[n1];
+    for (i = 0; i < n1; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+}
